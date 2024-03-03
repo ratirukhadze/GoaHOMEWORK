9519c59ee749659817033dd4607c40b62545881b
“rati <ratirukhadzeb@gmail.com>
2024-03-03 22:13:36 +0400

    commit

---
+++ day 5/homework/classwork/homework.py
@@ -0,0 +1,28 @@
+#weather
+city = 'batumi' 
+
+
+weather =  ' weather is good in '
+
+
+
+
+print( weather     +       city )
+
+#city
+
+live = ' i live in '
+
+print(live + city )
+
+#similar
+
+similar = ' similar '
+
+laptop = ' laptop '
+
+have = ' i have '
+
+friend = ' as my friend '
+
+print(have       +      similar  +      laptop  +     friend )
